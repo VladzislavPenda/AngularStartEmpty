065559f72d20224aafd17628ce664a702db8653d
User <User@DESKTOP-R0BLQ2C>
2021-02-25 23:37:25 +0300

    Add project files.

---
+++ AngularStartEmpty/Models/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using AngularStartEmpty.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloAngularApp.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
